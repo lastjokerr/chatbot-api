098b3834c5ce8aa92af2e2731bdf14611e44ad0b
Ran <ranwlive@outlook.com>
2023-06-22 00:13:37 +0800

    ran,feat:init ddd project

---
+++ chatbot-api-interfaces/src/main/java/cn/ran/chatbot/api/ApiApplication.java
@@ -0,0 +1,15 @@
+package cn.ran.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ran
+ * @description 启动入口
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/cn/ran/chatbot/api/test/ApiTest.java
@@ -0,0 +1,10 @@
+package cn.ran.chatbot.api.test;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTest {
+    public static void main(String[] args) {
+
+    }
+}
